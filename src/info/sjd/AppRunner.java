94594c4d115439aebe207c5658aefc213b588044
Vitaliy-tyma <vitaliy.tymashkov@gmail.com>
2018-10-17 15:20:46 +0300

    Initial commit

---
+++ src/info/sjd/AppRunner.java
@@ -0,0 +1,11 @@
+package info.sjd;
+
+import info.sjd.view.Window;
+
+public class AppRunner {
+
+	public static void main(String[] args) {
+		Window window = new Window("SEARCH WINDOW");
+	}
+
+}
---
+++ src/info/sjd/view/Window.java
@@ -0,0 +1,7 @@
+package info.sjd.view;
+
+import javax.swing.JFrame;
+
+public class Window extends JFrame {
+
+}
