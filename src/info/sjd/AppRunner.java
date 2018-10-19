91a6d6c09ed6d30384d4a2fce44ea7d55cbb1aa0
Vitaliy-tyma <vitaliy.tymashkov@gmail.com>
2018-10-19 13:27:08 +0300

    Error

--- src/info/sjd/AppRunner.java
+++ src/info/sjd/AppRunner.java
@@ -5,9 +5,14 @@ import info.sjd.view.Window;
 public class AppRunner {
 
 	public static void main(String[] args) {
-		
-		//Window window = new Window();
-		
+
+		// Window window = new Window();
+
 		Window.launch(args);
 	}
 }
+/*
+ * Error: class info.sjd.AppRunner is not a subclass of
+ * javafx.application.Application at
+ * javafx.graphics/javafx.application.Application.launch(Unknown Source)
+ */
